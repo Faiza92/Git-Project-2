619faa073b33dd4d7c9355e7897308046fe18091
Faiza <faiza.kabir@ryerson.ca>
2015-10-22 19:09:11 -0400

    added line

--- Git Project/src/Git Project.cpp
+++ Git Project/src/Git Project.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Git.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -11,5 +11,7 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout <<"lol what is  this";//detect change?
+	cout <<"hi";
 	return 0;
 }
